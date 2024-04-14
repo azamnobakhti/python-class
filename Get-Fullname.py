62400c7448ca725b8603034d15c6aec16acbba2a
fariborzfallahzadeh <fariborz.fallahzadeh@gmail.com>
2024-04-14 17:46:31 +0330

    Python Code Commit

---
+++ Get-Fullname.py
@@ -0,0 +1,8 @@
+# Get the first name from the user
+first_name = input("Please enter your first name: ")
+
+# Get the last name from the user
+last_name = input("Please enter your last name: ")
+
+# Print the full name
+print("Your full name is:", first_name, last_name)
